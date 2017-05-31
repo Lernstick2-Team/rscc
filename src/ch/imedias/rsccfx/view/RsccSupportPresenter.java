c85af8cc09ac6f496c953f5e016c8598c428a634
martinfrancois <fracoisma@hotmail.com>
2017-05-31 17:16:13 +0200

    removed wrongly place comment

--- src/ch/imedias/rsccfx/view/RsccSupportPresenter.java
+++ src/ch/imedias/rsccfx/view/RsccSupportPresenter.java
@@ -81,9 +81,6 @@ public class RsccSupportPresenter implements ControlledPresenter {
 
   }
 
-  /**
-   * Updates the validation image after every key pressed.
-   */
   private void attachEvents() {
     view.connectBtn.setOnAction(event -> model.connectToUser());
 
