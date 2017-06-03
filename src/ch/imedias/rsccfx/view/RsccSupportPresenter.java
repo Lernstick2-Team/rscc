563f1f0b6c267968d4d816aa18135fe0ba3ed825
jpduloch <roland.mosimann@students.fhnw.ch>
2017-06-03 22:09:36 +0200

    This makes the StatusBox workin

--- src/ch/imedias/rsccfx/view/RsccSupportPresenter.java
+++ src/ch/imedias/rsccfx/view/RsccSupportPresenter.java
@@ -82,7 +82,10 @@ public class RsccSupportPresenter implements ControlledPresenter {
   }
 
   private void attachEvents() {
-    view.connectBtn.setOnAction(event -> model.connectToUser());
+    view.connectBtn.setOnAction(event -> {
+      Thread thread = new Thread(model::connectToUser);
+      thread.start();
+    });
 
     // formats the key while typing
     StringProperty key = view.keyFld.textProperty();
@@ -138,7 +141,8 @@ public class RsccSupportPresenter implements ControlledPresenter {
     // make it possible to connect by pressing enter
     view.keyFld.setOnKeyPressed(ke -> {
       if (ke.getCode() == KeyCode.ENTER && keyUtil.isKeyValid()) {
-        model.connectToUser();
+        Thread thread = new Thread(model::connectToUser);
+        thread.start();
       }
     });
 
