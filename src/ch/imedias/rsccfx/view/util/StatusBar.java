14b5f217bdba8fac8a275306ef9ef0f1711cdbf3
martinfrancois <fracoisma@hotmail.com>
2017-06-13 14:47:05 +0200

    added statusbar class implementation basic and renamed css tags

--- src/ch/imedias/rsccfx/view/util/StatusBar.java
+++ src/ch/imedias/rsccfx/view/util/StatusBar.java
@@ -1,7 +1,28 @@
 package ch.imedias.rsccfx.view.util;
 
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
 /**
  * Created by François Martin on 13.06.17.
  */
-public class StatusBar {
+public class StatusBar extends HBox {
+
+  StringProperty status;
+  StringProperty cssTag;
+
+  final Label statusLbl = new Label();
+
+  public StatusBar(StringProperty status, StringProperty cssTag) {
+    this.status = status;
+    this.cssTag = cssTag;
+    this.getStyleClass().add("statusBar");
+    this.getChildren().add(statusLbl);
+    statusLbl.getStyleClass().add("statusLbl");
+  }
+
+
+
+
 }
