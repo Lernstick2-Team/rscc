88ebd01b192a370e789a3d1db49b99d5f7e99a2d
martinfrancois <fracoisma@hotmail.com>
2017-06-13 15:05:51 +0200

    removed unnecessary stuff and put it all in one method: setStatusProperties

--- src/ch/imedias/rsccfx/view/util/StatusBar.java
+++ src/ch/imedias/rsccfx/view/util/StatusBar.java
@@ -9,43 +9,22 @@ import javafx.scene.layout.HBox;
  */
 public class StatusBar extends HBox {
 
-  StringProperty status;
-  StringProperty cssTag;
-
   final Label statusLbl = new Label();
 
   public StatusBar() {
     this.getStyleClass().add("statusBar");
     this.getChildren().add(statusLbl);
     statusLbl.getStyleClass().add("statusLbl");
-    setupBindings();
-    attachEvents();
   }
 
-  public void setupBindings(){
+  public void setStatusProperties(StringProperty status, StringProperty styleClass){
     statusLbl.textProperty().bind(status);
-  }
-
-  public void attachEvents(){
-    cssTag.addListener((observable, oldValue, newValue) -> {
+    styleClass.addListener((observable, oldValue, newValue) -> {
       if (oldValue != newValue) {
         this.getStyleClass().clear();
         this.getStyleClass().add(newValue);
       }
     });
   }
-
-  public void setStatus(StringProperty status){
-    this.status.unbind();
-    this.status.bind(status);
-  }
-
-  public void setCssTag(StringProperty cssTag){
-    this.cssTag.unbind();
-    this.cssTag.bind(cssTag);
-  }
-
-
-
 
 }
