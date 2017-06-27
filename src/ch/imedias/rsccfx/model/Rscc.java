593b6462cb6fd74a99e09bdee597dfe81c386aec
jpduloch <roland.mosimann@students.fhnw.ch>
2017-06-27 16:05:26 +0200

    Created methods to read P2P-Port from keyserver & download keys

--- src/ch/imedias/rsccfx/model/Rscc.java
+++ src/ch/imedias/rsccfx/model/Rscc.java
@@ -4,13 +4,18 @@ import ch.imedias.rsccfx.localization.Strings;
 import ch.imedias.rsccfx.model.connectionutils.Rscccfp;
 import ch.imedias.rsccfx.model.connectionutils.RunRudp;
 import ch.imedias.rsccfx.model.util.KeyUtil;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.function.UnaryOperator;
@@ -117,6 +122,8 @@ public class Rscc {
   private final IntegerProperty proxyPort = new SimpleIntegerProperty();
   private final IntegerProperty stunServerPort = new SimpleIntegerProperty();
   private final BooleanProperty forcingServerMode = new SimpleBooleanProperty(false);
+
+  private int p2pPort;
 
   //States
   private final BooleanProperty vncSessionRunning = new SimpleBooleanProperty(false);
@@ -303,16 +310,68 @@ public class Rscc {
    * Sets up the server with use.sh.
    */
   private void keyServerSetup() {
-    String command = systemCommander.commandStringGenerator(
-        pathToResourceDocker, "use.sh", getKeyServerIp(), getKeyServerHttpPort());
-    SystemCommanderReturnValues returnValues = systemCommander.executeTerminalCommand(command);
+    readP2pPortFromKeyserver();
+    readKeysFromKeyserver();
 
-    if (returnValues.getExitCode() != 0) {
-      LOGGER.severe("Command failed: " + command + " ExitCode: " + returnValues.getExitCode());
-      return;
-    }
     isSshRunning.setValue(true);
   }
+
+
+  /**
+   * Read P2P Port from Keyserver.
+   */
+  private void readP2pPortFromKeyserver() {
+    String urlString = "http://" + getKeyServerIp() + ":" + getKeyServerHttpPort() + "/port";
+    try {
+      URL oracle = new URL(urlString);
+      URLConnection yc = oracle.openConnection();
+      BufferedReader in = new BufferedReader(new InputStreamReader(
+          yc.getInputStream()));
+      String inputLine = in.readLine();
+
+      if (inputLine != null) {
+        p2pPort = Integer.parseInt(inputLine);
+        LOGGER.info("Received  and set P2P-Ports to: " + p2pPort);
+      }
+
+      in.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+  /**
+   * Read KeyFiles.
+   */
+  private void readKeysFromKeyserver() {
+    String[] keyNames = {"create", "get", "delete"};
+    for (String keyName : keyNames) {
+      try {
+        String urlString = "http://" + getKeyServerIp() + ":" + getKeyServerHttpPort()
+            + "/keys/" + keyName + ".key";
+        File a = new File(pathToResourceDocker + "/keys/" + keyName + ".key");
+        a.delete();
+
+        String outputFile = pathToResources + "";
+        URL website = new URL(urlString);
+        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+        FileOutputStream fos = new FileOutputStream(
+            pathToResourceDocker + "/keys/" + keyName + ".key");
+
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        a.setReadable(false, false);
+        a.setReadable(true, true);
+        a.setWritable(false, false);
+        a.setExecutable(false, false);
+        a.setExecutable(true, true);
+
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
 
 
   /**
