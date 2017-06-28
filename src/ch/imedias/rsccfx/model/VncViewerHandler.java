20379cbe805ab27844e7b4dd189c38437591317f
Patrick Wigger <pwg@Patricks-MacBook-Air.local>
2017-06-29 00:09:36 +0200

    First steps into a java viewer

--- src/ch/imedias/rsccfx/model/VncViewerHandler.java
+++ src/ch/imedias/rsccfx/model/VncViewerHandler.java
@@ -1,9 +1,6 @@
 package ch.imedias.rsccfx.model;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import com.tigervnc.vncviewer.VncViewer;
 import java.util.logging.Logger;
 
 
@@ -15,11 +12,11 @@ public class VncViewerHandler {
       Logger.getLogger(VncViewerHandler.class.getName());
   private final Rscc model;
   private final CommandHandler command;
-  private Process process;
+  private VncViewer viewer;
+
 
   /**
    * Constructor to instantiate a VNCViewer.
-   *
    * @param model The one and only model.
    */
   public VncViewerHandler(Rscc model) {
@@ -35,75 +32,27 @@ public class VncViewerHandler {
    * @param hostAddress Address to connect to.
    */
   public void startVncViewerConnecting(String hostAddress, Integer vncViewerPort) {
-    Thread startViewerProcessThread = new Thread() {
+    Thread startConnecting = new Thread() {
       public void run() {
-        try {
-          LOGGER.info("Starting VNC Viewer Connection");
+        LOGGER.info("Starting VNC Viewer Connection");
+        String[] args = {hostAddress + ":" + vncViewerPort};
+        startVncViewer(args);
+        LOGGER.info("Starting VNCViewer with command: " + command);
+        model.setVncViewerProcessRunning(true);
 
-          String command = getViewerCommand(hostAddress, vncViewerPort, false);
-
-          LOGGER.info("Starting VNCViewer with command: " + command);
-
-          process = model.getSystemCommander().startProcess(command);
-
-          model.setVncViewerProcessRunning(true);
-
-          InputStream errorStream = process.getErrorStream();
-          BufferedReader errorReader = new BufferedReader(new InputStreamReader(errorStream));
-          String errorString;
-          while (process.isAlive()) {
-            errorString = errorReader.readLine();
-
-            if (errorString != null && (errorString.contains("Connection refused")
-                || errorString.contains("Usage"))) {
-              LOGGER.info("Detected: Viewer failed to connect");
-              killVncViewerProcess();
-            }
-
-            if (errorString != null && errorString.contains("Connected to RFB server")) {
-              LOGGER.info("Detected: Viewer connected sucessfully");
-              model.setVncSessionRunning(true);
-            }
-          }
-
-          LOGGER.info("VNC - Viewer process has ended");
-          if (model.isVncSessionRunning()) {
-            model.setStatusBarKeyInput(model.strings.statusBarConnectionClosed,
-                model.STATUS_BAR_STYLE_INITIALIZE);
-          }
-          model.setVncSessionRunning(false);
-          model.setVncViewerProcessRunning(false);
-
-          errorStream.close();
-
-        } catch (IOException e) {
-          LOGGER.info(e.getMessage());
+        LOGGER.info("VNC - Viewer process has ended");
+        if (model.isVncSessionRunning()) {
+          model.setStatusBarKeyInput(model.strings.statusBarConnectionClosed,
+              model.STATUS_BAR_STYLE_INITIALIZE);
         }
+        model.setVncSessionRunning(false);
+        model.setVncViewerProcessRunning(false);
 
         LOGGER.info("Ending VNC Viewer Thread ");
       }
     };
+    startConnecting.start();
 
-    startViewerProcessThread.start();
-  }
-
-  private String getViewerCommand(String hostAddress, Integer vncViewerPort, boolean listenMode) {
-    StringBuilder commandArray = new StringBuilder();
-    commandArray.append(command.getVncViewer());
-    if (listenMode) {
-      commandArray.append(" ").append(command.getVncViewerListen());
-    }
-    commandArray.append(" ").append(command.getVncViewerCompression());
-    commandArray.append(" ").append(Integer.toString((int) model.getVncCompression()));
-    commandArray.append(" ").append(command.getVncViewerQuality());
-    commandArray.append(" ").append(Integer.toString((int) model.getVncQuality()));
-    if (model.getVncBgr233()) {
-      commandArray.append(" ").append(command.getVncViewerBgr233());
-    }
-    if (!listenMode) {
-      commandArray.append(" ").append(hostAddress + "::" + vncViewerPort);
-    }
-    return commandArray.toString();
   }
 
 
@@ -111,43 +60,20 @@ public class VncViewerHandler {
    * Starts this VNCViewer listening on localhost.
    */
   public void startVncViewerListening() {
-
-    Thread startViewerProcessThread = new Thread() {
+    Thread startListening = new Thread() {
       public void run() {
+        String[] args = {"-listen"};
+        startVncViewer(args);
         LOGGER.info("Starting VNC Viewer listening Thread ");
         model.setVncViewerProcessRunning(true);
-        try {
-
-          String command = getViewerCommand(null, null, true);
-
-          LOGGER.info("Starting VNCViewer with command: " + command);
-
-          process = model.getSystemCommander().startProcess(command);
-
-          InputStream errorStream = process.getErrorStream();
-          BufferedReader errorReader = new BufferedReader(new InputStreamReader(errorStream));
-          String errorString;
-
-          while (process.isAlive()) {
-            errorString = errorReader.readLine();
-
-            if (errorString != null && errorString.contains("Connected to RFB server")) {
-              LOGGER.info("Detected: Server has connected");
-              model.setVncSessionRunning(true);
-            }
-          }
-
-          model.setVncViewerProcessRunning(false);
-          model.setVncSessionRunning(false);
-          errorStream.close();
-          LOGGER.info("VNC - Viewer process has ended");
-
-        } catch (IOException e) {
-          LOGGER.info(e.getMessage());
-        }
+        LOGGER.info("Starting VNCViewer with command: " + command);
+        model.setVncSessionRunning(true);
+        model.setVncViewerProcessRunning(false);
+        model.setVncSessionRunning(false);
       }
     };
-    startViewerProcessThread.start();
+    startListening.start();
+
   }
 
   /**
@@ -156,9 +82,21 @@ public class VncViewerHandler {
 
   public void killVncViewerProcess() {
     LOGGER.info("Stopping VNC-Viewer Process");
-    process.destroy();
     model.setVncViewerProcessRunning(false);
     model.setVncSessionRunning(false);
+    if (viewer != null) {
+      viewer.exit(0);
+    }
+  }
+
+  private void startVncViewer(String[] args) {
+    if (args == null) {
+      String[] defaultArgs = {"192.168.192.112"};
+      viewer = new VncViewer(defaultArgs);
+    } else {
+      viewer = new VncViewer(args);
+    }
+    viewer.start();
   }
 
 }
